2e89a6b9fcc6de2433c8d1c0df0a1680c0185891
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-05-06 11:35:32 -0400

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :plants do
-      resources :logs
-    end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :plants do
+    resources :logs
+  end
 end
