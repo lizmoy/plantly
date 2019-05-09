73fc5ece0b7e25e3712bcf605e004fe184167ca6
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-05-09 12:00:54 -0400

    fix application rb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Plantly
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.enable_dependency_loading = true
     config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
